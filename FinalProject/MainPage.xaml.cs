56ed8804d334e37c1b3c1d1a58aca292e616c09f
mervka <mervekarslan1@gmail.com>
2025-12-24 19:52:26 +0300

    Eksik olan dosyalar eklendi

---
+++ FinalProject/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using FinalProject.ViewModels;
+
+namespace FinalProject;
+
+public partial class MainPage : ContentPage
+{
+    private readonly MainViewModel _viewModel;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        
+        _viewModel = new MainViewModel();
+        BindingContext = _viewModel;
+    }
+}
